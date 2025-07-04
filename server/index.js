5f9dc5894d9003ec833c4e893193e147275549fe
karan <kj382542@gmail.com>
2025-07-04 14:53:16 +0530

    error solve

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { authRoutes } = require("./src/Routes/User-Route");
 const Connection = require("./config/db");
 const cookieParser = require("cookie-parser");
-const cors = require("cors")
+const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -11,23 +11,27 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "http://192.168.1.42:5173",
+
     credentials: true,
   },
 });
 
-app.use(cors({
-   origin: "http://localhost:5173",
+app.use(
+  cors({
+    //  origin: "http://localhost:5173",
+    origin: "http://192.168.1.42:5173",
     credentials: true,
-}))
-
-
+  })
+);
 
 io.on("connection", (socket) => {
-  console.log("connection on  socket ")
-  socket.on("message-user" ,(input) =>{
-    console.log("new user message "  ,input)
-  })
+  console.log("connection on  socket  1");
+  socket.on("message-user", (input) => {
+    console.log("new user message ", input);
+    io.emit("message-user", input);
+  });
 });
 
 app.get("/", (req, res) => {
--- server/src/Models/User-Model.js
+++ server/src/Models/User-Model.js
@@ -15,6 +15,6 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("user", UserSchema);
+const User = mongoose.model("userAuthChat-App", UserSchema);
 
 module.exports = User;
